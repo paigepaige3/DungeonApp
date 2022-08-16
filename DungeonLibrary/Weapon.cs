6538531e5fd99851cd7644ef987ef36a8bef56a4
paigepaige3 <paigescott3@outlook.com>
2022-08-16 15:47:08 -0500

    weapons and characters created

---
+++ DungeonLibrary/Weapon.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class Weapon
+    {
+        //fields
+        private int _minDamage;
+        private int _maxDamage;
+        private string _weaponName;
+        private int _bonusHitChance;
+        private bool _isTwoHanded;
+
+        //props
+        public int MinDamage
+        {
+            get { return _minDamage; }
+            set
+            {
+                if (value > 0)
+                {
+                    _minDamage = value;
+                }
+                else
+                {
+                    _minDamage = 1;
+                }
+            }
+        }
+        public int MaxDamage
+        {
+            get { return _maxDamage; }
+            set
+            {
+                if (value > 0)
+                {
+                    _maxDamage = value;
+                }
+                else
+                {
+                    _maxDamage = 1;
+                }
+            }
+        }
+        public string WeaponName
+        {
+            get { return _weaponName; }
+            set { _weaponName = value; }
+        }
+        public int BonusHitChance
+        {
+            get { return _bonusHitChance; }
+            set
+            {
+                if (value > 0)
+                {
+                    _bonusHitChance = value;
+                }
+                else
+                {
+                    _bonusHitChance = 1;
+                }
+            }
+        }
+        public bool IsTwoHanded
+        {
+            get { return _isTwoHanded; }
+            set { _isTwoHanded = value; }
+        }
+
+        //collect
+        public Weapon(int minDamage, int maxDamage, string weaponName, int bonusHitChance, bool isTwoHanded)
+        {
+            MaxDamage = maxDamage;
+            MinDamage = minDamage;
+            WeaponName = weaponName;
+            BonusHitChance = bonusHitChance;
+            IsTwoHanded = isTwoHanded;
+        }
+        public Weapon()
+        {
+
+        }
+        public override string ToString()
+        {
+            return string.Format("" +
+                "Min Damage: {0}" +
+                "Max Damage: {1}" +
+                "Name: {3}" +
+                "Bonus Hit Chance: {4}" +
+                "Two Handed: {5}",
+                MinDamage,
+                MaxDamage,
+                WeaponName,
+                BonusHitChance,
+                IsTwoHanded);
+        }
+    }
+}
